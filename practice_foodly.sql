6bb46ffb8c55ecbf9739edee505db751da452741
Jeremy-Cloarec <jeremycloarec@msn.com>
2025-03-27 19:28:53 +0100

    Table relation with JOIN

--- practice_foodly.sql
+++ practice_foodly.sql
@@ -1,15 +1,11 @@
 SHOW DATABASES;
-
 USE foodly;
-
 SELECT * FROM aliment;
-
 SELECT * FROM utilisateur;
-
+SELECT * FROM langue;
+SELECT * FROM utilisateur_aliment;
 SELECT nom FROM utilisateur;
-
 SHOW COLUMNS FROM aliment;
-
 SHOW TABLES;
 
 -- SHOW COLUMNS FROM utilisateur;
@@ -108,7 +104,56 @@ SELECT * FROM non_bio_aliments_vw;
 -- Un utiisateur peut scanner plusieus aliments et un aliment peut être scanné par plusieurs utilisateurs
 
 /**Relation un à plusieurs
-* 
-*  
-* 
-* */
+Chaque utilisateur est relié à une langue. Et chaque langue peut être reliée à plusieurs utilisateurs
+*/
+
+-- JOIN
+SELECT *
+FROM utilisateur
+    JOIN langue ON utilisateur.langue_id = langue.id;
+
+SELECT *
+FROM utilisateur
+    JOIN langue ON utilisateur.langue_id = langue_id
+WHERE (
+        utilisateur.email LIKE "%gmail.com"
+    )
+    AND (langue_id = 1);
+
+SELECT utilisateur.id, UPPER(utilisateur.nom) AS "NOM", utilisateur.prenom, utilisateur.email, langue.nom AS "LANGUE"
+FROM utilisateur
+    JOIN langue ON utilisateur.langue_id = langue.id
+WHERE (
+        utilisateur.email LIKE "%gmail%"
+    )
+    AND (langue.id = 1)
+    ORDER BY utilisateur.id DESC;
+
+-- Nom de famille de tous les utilisateurs ayant sélectionné le français
+SELECT UPPER(utilisateur.nom) as "NOM", langue.nom AS "LANGUE"  FROM utilisateur
+JOIN langue ON utilisateur.langue_id = langue.id
+WHERE langue.id = 1;
+
+/** Relations de plusieurs à plusieurs
+Stocker tous les aliments qui ont été scanné par un utilisateur
+=> un même utilisateur peut stocker plusieurs aliments scannés
+=> un aliment peut être scanné par plusieurs utilisateurs 
+
+Relation many to many. Mais SQL ne sait que stocker une valeur par champs. 
+Table utilisateur_aliment = sert à stocker des relations entre un utilisateur et un aliment
+Convention : {table1}_{table2}
+ */
+
+-- Commande pour relier tous les utilisateurs aux aliments qu'ils ont scannés
+SELECT *
+FROM utilisateur
+JOIN utilisateur_aliment ON (utilisateur.id = utilisateur_aliment.utilisateur_id )
+JOIN aliment ON (aliment.id = utilisateur_aliment.aliment_id); 
+
+-- Voir tous les aliments sélectionnés par les utilisateurs dont adress email est une adress gmail
+
+SELECT *
+FROM utilisateur
+JOIN utilisateur_aliment ON (utilisateur.id = utilisateur_aliment.utilisateur_id)
+JOIN aliment ON (aliment.id = utilisateur_aliment.aliment_id)
+WHERE utilisateur.email LIKE "%gmail%";
